f299d525a06636aa1f565396e19bca1545fa2fb3
Dal-Pi <kimsh707@naver.com>
2017-07-27 01:33:05 +0900

    [170727] re-define data structure

--- app/src/main/java/com/kania/todostack3/data/SubjectData.java
+++ app/src/main/java/com/kania/todostack3/data/SubjectData.java
@@ -5,8 +5,72 @@ package com.kania.todostack3.data;
  */
 
 public class SubjectData {
+
+    public static final int NON_SAVED_ID = -1;
+    public static final String DEFAULT_COLOR = "#FF555555";
+
+    //builder member
     public int id;
-    public String subjectName;
-    public int color;
     public int order;
+    public String subjectName;
+    //non-builder member
+    public String color;
+
+    public SubjectData(Builder builder) {
+        this.id = builder.id;
+        this.order = builder.order;
+        this.subjectName = builder.subjectName;
+        this.color = builder.color;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getColorString() {
+        return color;
+    }
+    public void setColorString(String color) {
+        this.color = color;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+    public void setOrder(int order) {
+        this.order = order;
+    }
+
+    public static class Builder {
+        public int id;
+        public int order;
+        public String subjectName;
+        public String color;
+
+        public Builder(int id, int order, String subjectName) {
+            this.id = id;
+            this.order = order;
+            this.subjectName = subjectName;
+            this.color = "#FF555555";
+        }
+
+        public Builder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public SubjectData build() {
+            return new SubjectData(this);
+        }
+    }
 }
--- app/src/main/java/com/kania/todostack3/data/TodoData.java
+++ app/src/main/java/com/kania/todostack3/data/TodoData.java
@@ -1,20 +1,144 @@
 package com.kania.todostack3.data;
 
+import java.util.ArrayList;
+
 /**
  * Created by Seonghan Kim on 2017-07-14.
  */
 
 public class TodoData {
 
-    public int id;
-    public String todoName;
-    public int subjectOrder;
-    public long date;
-    public int type;
-    public long timeFrom;
-    public long timeTo;
-    public String location;
-    public long created;
-    public long lastUpdated;
-    public boolean complete;
+    public static final int NON_SAVED_ID = -1;
+    public static final int NOT_DEFINED_DATE = -1;
+    public static final int PARENT_TODO_ID = -1;
+
+    //builder member
+    private int id;
+    private int subjectId;
+    private int parentId;
+    private String todoName;
+    private boolean isCompleted;
+    private long created;
+    private long lastUpdated;
+    private long targetDate;
+    private String location;
+    //non-builder member
+    private ArrayList<Integer> childrenIds;
+
+    private TodoData(Builder builder) {
+        this.id = builder.id;
+        this.subjectId = builder.subjectId;
+        this.parentId = builder.parentId;
+        this.todoName = builder.todoName;
+        this.isCompleted = builder.isCompleted;
+        this.created = builder.created;
+        this.lastUpdated = builder.lastUpdated;
+        this.targetDate = builder.targetDate;
+        this.location = builder.location;
+        this.childrenIds = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSubjectId() {
+        return subjectId;
+    }
+    public void setSubjectId(int subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    public void setCompleted(boolean completed) {
+        this.isCompleted = completed;
+    }
+
+    public String getTodoName() {
+        return todoName;
+    }
+    public void setId(String todoName) {
+        this.todoName = todoName;
+    }
+
+    public long getCreatedDate() {
+        return created;
+    }
+    public void setCreatedDate(long created) {
+        this.created = created;
+    }
+
+    public long getLastUpdatedDate() {
+        return lastUpdated;
+    }
+    public void setLastUpdatedDate(long lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public long getTargetDate() {
+        return targetDate;
+    }
+    public void setTargetDate(long targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public static class Builder {
+        private int id;
+        private int subjectId;
+        private int parentId;
+        private String todoName;
+        private boolean isCompleted = false;
+        private long created = NOT_DEFINED_DATE;
+        private long lastUpdated = NOT_DEFINED_DATE;
+        private long targetDate = NOT_DEFINED_DATE;
+        private String location;
+
+        public Builder(int id, int subjectId, int parentId, String todoName) {
+            this.id = id;
+            this.subjectId = subjectId;
+            this.parentId = parentId;
+            this.todoName = todoName;
+        }
+
+        public Builder completed(boolean isCompleted) {
+            this.isCompleted = isCompleted;
+            return this;
+        }
+
+        public Builder createdDate(long created) {
+            this.created = created;
+            return this;
+        }
+
+        public Builder updatedDate(long lastUpdated) {
+            this.lastUpdated = lastUpdated;
+            return this;
+        }
+
+        public Builder targetDate(long targetDate) {
+            this.targetDate = targetDate;
+            return this;
+        }
+
+        public Builder targetDate(String location) {
+            this.location = location;
+            return this;
+        }
+
+        public TodoData build() {
+            return new TodoData(this);
+        }
+    }
 }
